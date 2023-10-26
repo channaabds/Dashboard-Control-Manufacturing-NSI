131e62dde52c7c713f51e217c99760c97d8f7406
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2023-10-26 14:45:47 +0700

    implementasi api di laravel batal karena susah untuk dikonsumsi frontend

--- routes/api.php
+++ routes/api.php
@@ -19,20 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/api', function () {
-    return ['/dashboard-repair'];
-});
-
-Route::prefix('maintenance')->group(function () {
-    Route::get('/downtime', [MaintenanceController::class, 'currentDowntime']);
-    Route::get('/downtime-1', [MaintenanceController::class, 'lastDowntime']);
-    Route::get('/downtime-2', [MaintenanceController::class, 'beforeLastDowntime']);
-    Route::get('/data-downtime', [MaintenanceController::class, 'dataMachineRepairs']);
-    Route::get('/history-downtime', [MaintenanceController::class, 'historyDowntimes']);
-});
-
-Route::prefix('quality')->group(function () {
-    Route::get('ipqc', [ApiQualityController::class, 'ipqc']);
-    Route::get('oqc', [ApiQualityController::class, 'oqc']);
-});
